6991bf1760fae6800318969c4611632ebde5990e
Jordan Christiansen <xordspar0@gmail.com>
2021-06-11 14:39:11 -0500

    Add track images

--- track.lua
+++ track.lua
@@ -3,8 +3,15 @@ local track = {}
 function track.new()
 	local self = setmetatable({}, {__index = track})
 
-	self.segmentSize = 25
+	self.segmentSize = 32
 	self.segments = {'r', 'r', 'dr', 'd', 'dl', 'l', 'l', 'ul', 'u', 'ur'}
+
+	self.track_ud = love.graphics.newImage("res/track_ud.png")
+	self.track_rl = love.graphics.newImage("res/track_rl.png")
+	self.track_ur = love.graphics.newImage("res/track_ur.png")
+	self.track_ul = love.graphics.newImage("res/track_ul.png")
+	self.track_dr = love.graphics.newImage("res/track_dr.png")
+	self.track_dl = love.graphics.newImage("res/track_dl.png")
 
 	return self
 end
@@ -15,28 +22,28 @@ function track:draw()
 
 	for i, segment in ipairs(self.segments) do
 		if segment == "u" then
-			love.graphics.rectangle("line", x, y, l, l)
+			love.graphics.draw(self.track_ud, x, y)
 			y = y - l
 		elseif segment == "ur" then
-			love.graphics.polygon('line', x, y + l, x + l, y, x + l, y + l)
+			love.graphics.draw(self.track_ur, x, y)
 			x = x + l
 		elseif segment == "ul" then
-			love.graphics.polygon('line', x, y, x + l, y, x + l, y + l)
+			love.graphics.draw(self.track_ul, x, y)
 			y = y - l
 		elseif segment == "r" then
-			love.graphics.rectangle("line", x, y, l, l)
+			love.graphics.draw(self.track_rl, x, y)
 			x = x + l
 		elseif segment == "l" then
-			love.graphics.rectangle("line", x, y, l, l)
+			love.graphics.draw(self.track_rl, x, y)
 			x = x - l
 		elseif segment == "d" then
-			love.graphics.rectangle("line", x, y, l, l)
+			love.graphics.draw(self.track_ud, x, y)
 			y = y + l
 		elseif segment == "dr" then
-			love.graphics.polygon('line', x, y, x + l, y + l, x, y + l)
+			love.graphics.draw(self.track_dr, x, y)
 			y = y + l
 		elseif segment == "dl" then
-			love.graphics.polygon('line', x, y, x + l, y, x, y + l)
+			love.graphics.draw(self.track_dl, x, y)
 			x = x - l
 		end
 	end
